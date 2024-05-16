78598434f49c31c1c934f1cab427299d9bda0cef
garciaVarea <151222886+garciaVarea@users.noreply.github.com>
2024-05-16 11:09:48 +0200

    Delete mondo_getpost-master directory

--- mondo_getpost-master/README.md
+++
@@ -1,3 +0,0 @@
-# mondo_getpost
-mongo getpost juanjo db
-0.17 - Lectura Escritura Mongo
--- mondo_getpost-master/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.empresa.javafx_mongo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires mongo.java.driver;
-
-
-    opens com.empresa.javafx_mongo to javafx.fxml;
-    exports com.empresa.javafx_mongo;
-}
